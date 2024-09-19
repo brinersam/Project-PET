2758180a4e21f3aa112d486d492c3bc9a06d1e8d
brinersam <vladbudyachenko@mail.ru>
2024-09-19 21:16:18 +0300

    Create pet domain model

---
+++ src/ProjectPet.Domain/Models/Pet.cs
@@ -0,0 +1,45 @@
+﻿namespace ProjectPet.Domain.Models
+{
+    public class Pet
+    {
+        public Guid Id { get; private set; }
+        public string Name { get; private set; } = null!;
+        public string Species { get; private set; } = null!;
+        public string Description { get; private set; } = null!;
+        public string Breed { get; private set; } = null!;
+        public string Coat { get; private set; } = null!;
+        public string Health { get; private set; } = null!;
+        public string Address { get; private set; } = null!;
+        public string WeightKg { get; private set; } = null!;
+        public string Height { get; private set; } = null!;
+        public string PhoneNumber { get; private set; } = null!;
+        public bool Sterilized { get; private set; }
+        public bool Vaccinated { get; private set; }
+        public Status Status { get; private set; }
+        public DateOnly DateOfBirth { get; private set; }
+        public DateOnly CreatedOn { get; private set; }
+
+        private List<PaymentInfo> _paymentInfo = [];
+        public IReadOnlyList<PaymentInfo> PaymentInfo => _paymentInfo;
+
+        public void AddPaymentInfo(PaymentInfo paymentInfo)
+        {
+            _paymentInfo.Add(paymentInfo);
+        }
+    }
+
+    public class PaymentInfo
+    {
+        public string Title { get; private set; } = null!;
+        public string Description { get; private set; } = null!;
+    }
+
+    public enum Status
+    {
+        NotSet,
+        Requires_Care,
+        Looking_For_Home,
+        Home_Found
+    }
+}
+
