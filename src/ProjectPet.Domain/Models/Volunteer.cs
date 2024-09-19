131af20b22bc0067a7f2d31620d9805f24dec19a
brinersam <vladbudyachenko@mail.ru>
2024-09-19 21:46:16 +0300

    Creates volunteer model

---
+++ src/ProjectPet.Domain/Models/Volunteer.cs
@@ -0,0 +1,28 @@
+﻿namespace ProjectPet.Domain.Models
+{
+    public class Volunteer
+    {
+        public Guid Id { get; private set; }
+        public string FullName { get; private set; } = null!;
+        public string Email { get; private set; } = null!;
+        public string Description { get; private set; } = null!;
+        public int YOExperience { get; private set; }
+        public string PhoneNumber { get; private set; } = null!;
+        private List<Pet> _ownedPets = [];
+        private List<SocialNetwork> _socialNetworks = [];
+        private List<PaymentInfo> _paymentMethods = [];
+        public int PetsHoused => _ownedPets.Count(x => x.Status == Status.Home_Found);
+        public int PetsLookingForHome => _ownedPets.Count(x => x.Status == Status.Looking_For_Home);
+        public int PetsInCare => _ownedPets.Count(x => x.Status == Status.Requires_Care);
+        public IReadOnlyList<Pet> OwnedPets => _ownedPets;
+        public IReadOnlyList<SocialNetwork> SocialNetworks => _socialNetworks;
+        public IReadOnlyList<PaymentInfo> PaymentMethods => _paymentMethods;
+    }
+
+    public class SocialNetwork
+    {
+        public string Name { get; private set; } = null!;
+        public string Link { get; private set; } = null!;
+
+    }
+}
