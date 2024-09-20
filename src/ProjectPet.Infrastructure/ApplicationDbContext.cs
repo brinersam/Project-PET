2f778ea10dac5def8daeae7a5abc19ad853414b0
brinersam <vladbudyachenko@mail.ru>
2024-09-20 21:53:55 +0300

    Infrastructure Creates configurations Applies them in dbcontext

--- src/ProjectPet.Infrastructure/ApplicationDbContext.cs
+++ src/ProjectPet.Infrastructure/ApplicationDbContext.cs
@@ -7,11 +7,17 @@ namespace ProjectPet.Infrastructure
     public class ApplicationDbContext(IConfiguration _configuration) : DbContext
     {
         private const string DATABASE = "Database";
+
         protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
         {
             optionsBuilder.UseNpgsql(_configuration.GetConnectionString(DATABASE));
             optionsBuilder.UseSnakeCaseNamingConvention();
             optionsBuilder.UseLoggerFactory(CreateLoggerFactory());
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.ApplyConfigurationsFromAssembly(typeof(ApplicationDbContext).Assembly);
         }
 
         private ILoggerFactory CreateLoggerFactory()
---
+++ src/ProjectPet.Infrastructure/Configurations/PetConfiguration.cs
@@ -0,0 +1,71 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using ProjectPet.Domain.Models;
+using ProjectPet.Domain.Shared;
+
+namespace ProjectPet.Infrastructure.Configurations
+{
+    public class PetConfiguration : IEntityTypeConfiguration<Pet>
+    {
+        public void Configure(EntityTypeBuilder<Pet> builder)
+        {
+            builder.ToTable("pets");
+
+            builder.HasKey(e => e.Id);
+
+            builder.Property(e => e.Name)
+                .ConfigureString();
+
+            builder.Property(e => e.Species)
+                .ConfigureString();
+
+            builder.Property(e => e.Description)
+                .ConfigureString(Constants.STRING_LEN_MEDIUM);
+
+            builder.Property(e => e.Breed)
+                .ConfigureString();
+
+            builder.Property(e => e.Coat);
+
+            builder.Property(e => e.Health)
+                .ConfigureString(Constants.STRING_LEN_MEDIUM);
+
+            builder.Property(e => e.Address)
+                .ConfigureString(Constants.STRING_LEN_MEDIUM);
+
+            builder.Property(e => e.Weight);
+
+            builder.Property(e => e.Height);
+
+            builder.Property(e => e.PhoneNumber)
+                .ConfigureString();
+
+            builder.Property(e => e.IsSterilized)
+                .IsRequired();
+
+            builder.Property(e => e.IsVaccinated)
+                .IsRequired();
+
+            builder.Property(e => e.Status)
+                .IsRequired()
+                .HasConversion(
+                    status => status.ToString(),
+                    status => Enum.Parse<Status>(status)
+                );
+
+            builder.Property(e => e.DateOfBirth)
+                .IsRequired();
+
+            builder.Property(e => e.CreatedOn)
+                .IsRequired();
+
+            builder.OwnsMany(
+                e => e.PaymentMethods,
+                d => d.ToJson());
+
+            builder.OwnsMany(
+                e => e.Photos,
+                d => d.ToJson());
+        }
+    }
+}
