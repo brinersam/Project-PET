af5b395a1d54dd596400b9443fa5cdae2ecbfd56
brinersam <vladbudyachenko@mail.ru>
2024-09-25 20:59:17 +0300

    Refactors DTO

--- src/ProjectPet.Application/UseCases/CreateVolunteer/CreateVolunteerRequest.cs
+++ src/ProjectPet.Application/UseCases/CreateVolunteer/CreateVolunteerRequest.cs
@@ -2,30 +2,15 @@
 
 namespace ProjectPet.Application.UseCases.CreateVolunteer
 {
-    public record CreateVolunteerRequest
-    {
-        public CreateVolunteerRequest(string fullName, string email, string description, int yOExperience, string phonenumber, string phonenumberAreaCode, IEnumerable<Pet> ownedPets, IEnumerable<PaymentInfo>? paymentMethods, IEnumerable<SocialNetwork>? socialNetworks)
-        {
-            FullName = fullName;
-            Email = email;
-            Description = description;
-            YOExperience = yOExperience;
-            Phonenumber = phonenumber;
-            PhonenumberAreaCode = phonenumberAreaCode;
-            OwnedPets = ownedPets;
-            PaymentMethods = paymentMethods;
-            SocialNetworks = socialNetworks;
-        }
-
-        public string FullName { get; } = null!;
-        public string Email {get; } = null!;
-        public string Description {get; } = null!;
-        public int YOExperience { get; }
-        public string Phonenumber { get; } = null!;
-        public string PhonenumberAreaCode { get; } = null!;
-        public IEnumerable<Pet> OwnedPets { get; } = null!;
-        public IEnumerable<PaymentInfo>? PaymentMethods { get; } = null!;
-        public IEnumerable<SocialNetwork>? SocialNetworks { get; } = null!;
-
-    }
+    public record CreateVolunteerRequest(
+    string FullName,
+    string Email,
+    string Description,
+    int YOExperience,
+    string Phonenumber,
+    string PhonenumberAreaCode,
+    IEnumerable<Pet> OwnedPets,
+    IEnumerable<PaymentInfo>? PaymentMethods,
+    IEnumerable<SocialNetwork>? SocialNetworks)
+    { }
 }
