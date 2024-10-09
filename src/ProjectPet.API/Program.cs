cb1e5d9fb6ab2861bf7839f8a7d6a3cc191febe4
brinersam <vladbudyachenko@mail.ru>
2024-10-09 04:31:06 +0300

    Create softdelete interceptor and add it to DI

--- src/ProjectPet.API/Program.cs
+++ src/ProjectPet.API/Program.cs
@@ -6,6 +6,7 @@ using Serilog;
 using Serilog.Events;
 using ProjectPet.API.MIddlewares;
 using ProjectPet.Application.UseCases.Volunteers;
+using ProjectPet.Infrastructure.Interceptors;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -36,6 +37,7 @@ builder.Services.AddScoped<CreateVolunteerHandler>();
 builder.Services.AddScoped<UpdateVolunteerInfoHandler>();
 builder.Services.AddScoped<UpdateVolunteerPaymentHandler>();
 builder.Services.AddScoped<UpdateVolunteerSocialsHandler>();
+builder.Services.AddScoped<DeleteVolunteerHandler>();
 
 builder.Services.AddFluentValidationAutoValidation();
 builder.Services.AddValidatorsFromAssembly(typeof(IVolunteerRepository).Assembly);
---
+++ src/ProjectPet.Infrastructure/Interceptors/SoftDeleteInterceptor.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.EntityFrameworkCore.Diagnostics;
+using Microsoft.EntityFrameworkCore;
+using ProjectPet.Domain.Shared;
+
+namespace ProjectPet.Infrastructure.Interceptors
+{
+    public class SoftDeleteInterceptor : SaveChangesInterceptor
+    {
+        public override ValueTask<InterceptionResult<int>> SavingChangesAsync(
+            DbContextEventData eventData,
+            InterceptionResult<int> result,
+            CancellationToken cancellationToken = default)
+        {
+            if (eventData.Context is null)
+                return base.SavingChangesAsync(eventData, result, cancellationToken);
+
+            var entries = eventData.Context.ChangeTracker
+                .Entries()
+                .Where(e => e.State == EntityState.Deleted);
+
+            foreach (var entry in entries)
+            {
+                entry.State = EntityState.Modified;
+                if (entry.Entity is ISoftDeletable deletableEntry)
+                    deletableEntry.SetIsDeletedFlag(true);
+            }
+
+            return base.SavingChangesAsync(eventData, result, cancellationToken);
+        }
+    }
+}
