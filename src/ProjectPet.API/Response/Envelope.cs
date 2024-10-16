161d2bae81d511ea7e0a25c62cea30f9ea64e8ba
brinersam <vladbudyachenko@mail.ru>
2024-10-16 23:57:00 +0300

    Add envelope class to unify api return format

---
+++ src/ProjectPet.API/Response/Envelope.cs
@@ -0,0 +1,38 @@
+﻿namespace ProjectPet.API.Response
+{
+    public record Envelope
+    {
+        public object? Result { get; }
+        public DateTime? TimeGenerated { get; }
+        public object? Errors { get; } = null;
+        protected Envelope(object? result)
+        {
+            Result = result;
+            TimeGenerated = DateTime.Now;
+        }
+
+        public static Envelope Ok(object? result = null)
+            => new Envelope(result);
+
+        public static Envelope<T> Error<T>(IEnumerable<T> errors)
+            => Envelope<T>.Create(errors);
+    }
+
+    public record Envelope<T> : Envelope
+    {
+        new public List<T> Errors { get; }
+
+        private Envelope(object? result,IEnumerable<T> errors) : base(result)
+        {
+            Errors = errors.ToList();
+        }
+
+        public static Envelope<T> Create(IEnumerable<T> errors)
+            => new Envelope<T>(null, errors);
+    }
+
+    public record ResponseError(
+        string? ErrorCode,
+        string? ErrorMessage,
+        string? InvalidField);
+}
