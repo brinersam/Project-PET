5562281936d37c9da07d75e9fc19181a02d74401
brinersam <vladbudyachenko@mail.ru>
2024-11-26 14:33:18 +0300

    Updates bat files

--- bat/CreateMigrations.bat
+++ bat/CreateMigrations.bat
@@ -1,2 +1,4 @@
-cd ../src
-dotnet ef migrations add Initial -s ProjectPet.API -p ProjectPet.Infrastructure --context WriteDbContext
+cd ../
+dotnet ef migrations add InitialSpecies -s src/ProjectPet.Web/ProjectPet.Web.csproj -p src/Species/ProjectPet.Species.Infrastructure --context WriteDbContext
+dotnet ef migrations add InitialVolunteer -s src/ProjectPet.Web/ProjectPet.Web.csproj -p src/Volunteer/ProjectPet.Volunteer.Infrastructure --context WriteDbContext
+dotnet ef migrations add InitialAccounts -s src/ProjectPet.Web/ProjectPet.Web.csproj -p src/AccountsModule/ProjectPet.AccountsModule.Infrastructure --context AuthDbContext
--- bat/WipeMigrations.bat
+++ bat/WipeMigrations.bat
@@ -1,4 +1,14 @@
-cd ../src/ProjectPet.Infrastructure
-dir Migrations
-rmdir Migrations /s
+cd ../src/AccountsModule/ProjectPet.AccountsModule.Infrastructure
+rmdir Migrations /s /Q
 mkdir Migrations
+cd "%~dp0"
+
+cd ../src/Volunteer/ProjectPet.Volunteer.Infrastructure
+rmdir Migrations /s /Q
+mkdir Migrations
+cd "%~dp0"
+
+cd ../src/Species/ProjectPet.Species.Infrastructure
+rmdir Migrations /s /Q
+mkdir Migrations
+cd "%~dp0"
