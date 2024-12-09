0eac3fbb81b0dcd00088bc947944bf1658787c25
brinersam <vladbudyachenko@mail.ru>
2024-12-10 02:32:12 +0300

    Update user model and configuration

--- src/AccountsModule/ProjectPet.AccountsModule.Domain/User.cs
+++ src/AccountsModule/ProjectPet.AccountsModule.Domain/User.cs
@@ -7,20 +7,35 @@ namespace ProjectPet.AccountsModule.Domain;
 #pragma warning disable CS8618 // Non-nullable field must contain a non-null value when exiting constructor. Consider declaring as nullable.
 public class User : IdentityUser<Guid>
 {
-    public IReadOnlyList<PaymentInfo> PaymentInfos => _paymentInfos;
-    private List<PaymentInfo> _paymentInfos;
     public IReadOnlyList<SocialNetwork> SocialNetworks => _socialNetworks;
     private List<SocialNetwork> _socialNetworks;
+    public IReadOnlyList<Role> Roles => _roles;
+    private List<Role> _roles;
     public VolunteerAccount VolunteerData { get; private set; }
     public AdminAccount AdminData { get; private set; }
     public MemberAccount MemberData { get; private set; }
-#pragma warning restore CS8618 // Non-nullable field must contain a non-null value when exiting constructor. Consider declaring as nullable.
 
     public User(){} //efcore
 
+    public User(
+        string username,
+        string email,
+        MemberAccount? memberData = null,
+        AdminAccount? adminData = null,
+        VolunteerAccount? volunteerData = null)
+    {
+        UserName = username;
+        Email = email;
+
+        VolunteerData = volunteerData!;
+        MemberData = memberData!;
+        AdminData = adminData!;
+    }
+#pragma warning restore CS8618 // Non-nullable field must contain a non-null value when exiting constructor. Consider declaring as nullable.
+
     public void UpdatePaymentMethods(IEnumerable<PaymentInfo> infos)
     {
-        _paymentInfos = infos.ToList();
+        VolunteerData = new VolunteerAccount(infos.ToList(), VolunteerData.Experience, VolunteerData.Certifications);
     }
 
     public void UpdateSocialNetworks(IEnumerable<SocialNetwork> networks)
