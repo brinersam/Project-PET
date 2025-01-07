d435771a7db20f2bd615ef0916e88f63a15b5628
brinersam <vladbudyachenko@mail.ru>
2025-01-07 14:36:49 +0300

    Add background service for deleting expired softdelete records

---
+++ src/Volunteer/ProjectPet.Volunteer.Infrastructure/BackgroundServices/SoftDeleteCleanupService.cs
@@ -0,0 +1,75 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
+using Microsoft.Extensions.Options;
+using ProjectPet.Core.Abstractions;
+using ProjectPet.Core.Options;
+using ProjectPet.SharedKernel.Entities.AbstractBase;
+using ProjectPet.VolunteerModule.Domain.Models;
+using ProjectPet.VolunteerModule.Infrastructure.Database;
+
+namespace ProjectPet.VolunteerModule.Infrastructure.BackgroundServices;
+public class SoftDeleteCleanupService : BackgroundService
+{
+    private readonly ILogger<SoftDeleteCleanupService> _logger;
+    private readonly IServiceScopeFactory _serviceScopeFactory;
+    private readonly OptionsDb _options;
+
+    public SoftDeleteCleanupService(
+        ILogger<SoftDeleteCleanupService> logger,
+        IServiceScopeFactory serviceScopeFactory,
+        IOptions<OptionsDb> options)
+    {
+        _logger = logger;
+        _serviceScopeFactory = serviceScopeFactory;
+        _options = options.Value;
+    }
+
+    protected override async Task ExecuteAsync(CancellationToken stoppingToken)
+    {
+        await using var scope = _serviceScopeFactory.CreateAsyncScope();
+        var dbContext = scope.ServiceProvider.GetRequiredService<WriteDbContext>();
+        var unitOfWork = scope.ServiceProvider.GetRequiredService<IUnitOfWork>();
+
+        int cleanupFrequencyHours = 1000 * 60 * _options.SoftDeletedCleanupFrequencyHours;
+
+        _logger.LogInformation("{serviceName} was started!", nameof(SoftDeleteCleanupService));
+
+        while (stoppingToken.IsCancellationRequested == false)
+        {
+            await Task.Delay(cleanupFrequencyHours, stoppingToken);
+
+            _logger.LogInformation("{serviceName} is working...", nameof(SoftDeleteCleanupService));
+
+            var transaction = await unitOfWork.BeginTransactionAsync(stoppingToken);
+            int cleanedUpEntities = 0;
+            RemoveExpiredSoftDeletedEntitiesFromSet<Volunteer>(ref cleanedUpEntities, dbContext);
+            RemoveExpiredSoftDeletedEntitiesFromSet<Pet>(ref cleanedUpEntities, dbContext);
+            await unitOfWork.SaveChangesAsync(stoppingToken);
+            transaction.Commit();
+
+            _logger.LogInformation(
+                "{serviceName} cleaned up {x} entires!",
+                nameof(SoftDeleteCleanupService),
+                cleanedUpEntities);
+        }
+
+        await Task.CompletedTask;
+    }
+
+    private void RemoveExpiredSoftDeletedEntitiesFromSet<T>(ref int cleanedUpEntities, DbContext context) where T: SoftDeletableEntity
+    {
+        var set = context.Set<T>();
+
+        foreach (T entity in set)
+        {
+            var lifetimeDays = (DateTime.UtcNow - entity.DeletionDate).Days;
+            if (lifetimeDays < _options.SoftDeletedMaxLifeTimeDays)
+                continue;
+
+            cleanedUpEntities++;
+            set.Remove(entity);
+        }
+    }
+}
