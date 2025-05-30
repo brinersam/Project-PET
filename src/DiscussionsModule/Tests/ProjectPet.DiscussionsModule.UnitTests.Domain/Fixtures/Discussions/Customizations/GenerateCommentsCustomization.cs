90279fe6c308c3df8e3df4bd459547113405a51b
brinersam <vladbudyachenko@mail.ru>
2025-05-30 03:04:48 +0300

    Добавление id в сообщения через рефлексию для тестов

--- src/DiscussionsModule/Tests/ProjectPet.DiscussionsModule.UnitTests.Domain/Fixtures/Discussions/Customizations/GenerateCommentsCustomization.cs
+++ src/DiscussionsModule/Tests/ProjectPet.DiscussionsModule.UnitTests.Domain/Fixtures/Discussions/Customizations/GenerateCommentsCustomization.cs
@@ -1,5 +1,6 @@
 ﻿using AutoFixture;
 using ProjectPet.DiscussionsModule.Domain.Models;
+using System.Reflection;
 
 namespace ProjectPet.DiscussionsModule.UnitTests.Domain.Fixtures.Discussions.Customizations;
 public class GenerateCommentsCustomization : ICustomization
@@ -9,6 +10,19 @@ public class GenerateCommentsCustomization : ICustomization
         var discussion = fixture.Freeze<Discussion>();
 
         GenerateComments(fixture, discussion);
+        AddIdToComments(fixture, discussion);
+    }
+
+    private static void AddIdToComments(IFixture fixture, Discussion discussion)
+    {
+        foreach (var comment in discussion.Messages)
+        {
+            Type type = typeof(Message);
+            PropertyInfo idProperty = type.BaseType?.GetProperty(nameof(Message.Id), BindingFlags.Instance | BindingFlags.Public)!;
+
+            var setMethod = idProperty.GetSetMethod(true);
+            setMethod.Invoke(comment, new object[] { fixture.Create<Guid>() });
+        }
     }
 
     private static void GenerateComments(IFixture fixture, Discussion discussion)
