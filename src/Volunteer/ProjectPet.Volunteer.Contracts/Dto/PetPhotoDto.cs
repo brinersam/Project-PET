3c3558193f5c1088069d6825ce1a9467d7c4df9b
brinersam <vladbudyachenko@mail.ru>
2025-07-02 21:36:36 +0300

    Рефактор добавление presigned url на скачивание при запросах к питомцам

---
+++ src/Volunteer/ProjectPet.Volunteer.Contracts/Dto/PetPhotoDto.cs
@@ -0,0 +1,10 @@
+﻿namespace ProjectPet.VolunteerModule.Contracts.Dto;
+public record PetPhotoDto
+(
+    string FileId,
+    string BucketName,
+    string FileName,
+    string ContentType,
+    bool IsPrimary
+)
+{ }
---
+++ src/Volunteer/ProjectPet.Volunteer.Contracts/Responses/PetResponse.cs
@@ -0,0 +1,36 @@
+﻿using ProjectPet.Core.Requests;
+using ProjectPet.VolunteerModule.Contracts.Dto;
+
+namespace ProjectPet.VolunteerModule.Contracts.Responses;
+public record PetResponse(
+    Guid Id,
+    Guid VolunteerId,
+    Guid SpeciesID,
+    Guid BreedID,
+    string Name,
+    string Description,
+    string Coat,
+    DateOnly DateOfBirth,
+    string Status,
+    List<PetPhotoResponse> Photos) : IMapFromRequest<PetResponse, PetDto, IEnumerable<PetPhotoResponse>> // todo rename interface to IMapFrom
+{
+    public static PetResponse FromRequest(PetDto dto, IEnumerable<PetPhotoResponse> photos) 
+    {
+        return new PetResponse(
+            dto.Id,
+            dto.VolunteerId,
+            dto.SpeciesID,
+            dto.BreedID,
+            dto.Name,
+            dto.Description,
+            dto.Coat,
+            dto.DateOfBirth,
+            dto.Status,
+            photos.ToList());
+    }
+}
+
+public record PetPhotoResponse(string FileName,
+                               string FileId,
+                               string Url)
+{ }
