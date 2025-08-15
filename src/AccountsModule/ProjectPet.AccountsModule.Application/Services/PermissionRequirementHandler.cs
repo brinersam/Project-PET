37ceef55082dc8d2230d49e883869cf50b36c13a
brinersam <vladbudyachenko@mail.ru>
2025-08-16 01:28:37 +0300

    Фикс authorizationHandler

--- src/AccountsModule/ProjectPet.AccountsModule.Application/Services/PermissionRequirementHandler.cs
+++ src/AccountsModule/ProjectPet.AccountsModule.Application/Services/PermissionRequirementHandler.cs
@@ -2,6 +2,7 @@
 using Microsoft.AspNetCore.Http;
 using Microsoft.Extensions.DependencyInjection;
 using ProjectPet.AccountsModule.Application.Interfaces;
+using ProjectPet.AccountsModule.Domain;
 using ProjectPet.Framework.Authorization;
 
 namespace ProjectPet.AccountsModule.Application.Services;
@@ -9,11 +10,14 @@ namespace ProjectPet.AccountsModule.Application.Services;
 public class PermissionRequirementHandler : AuthorizationHandler<PermissionAttribute>
 {
     private readonly IHttpContextAccessor _httpContextAccessor;
+    private readonly IAuthRepository _authRepository;
 
     public PermissionRequirementHandler(
-        IHttpContextAccessor httpContextAccessor)
+        IHttpContextAccessor httpContextAccessor,
+        IAuthRepository authRepository)
     {
         _httpContextAccessor = httpContextAccessor;
+        _authRepository = authRepository;
     }
 
     protected override async Task HandleRequirementAsync(
@@ -27,13 +31,14 @@ public class PermissionRequirementHandler : AuthorizationHandler<PermissionAttri
         }
 
         var userScopedData = _httpContextAccessor.HttpContext.RequestServices.GetRequiredService<UserScopedData>();
-
-        if (userScopedData.Permissions?.Contains(requirement.Code) == false)
+        if (userScopedData.IsSuccess == false)
         {
             context.Fail();
             return;
         }
 
-        context.Succeed(requirement);
+        bool isRoleAuthorized = await _authRepository.DoesUserHavePermissionCodeAsync((Guid)userScopedData.UserId!, requirement.Code);
+        if (isRoleAuthorized)
+            context.Succeed(requirement);
     }
 }
